cb1594ae322cd6900ea8326328c4fd6280e76019
Gabriel Arcanjo dos Santos Ferreira <gabriel.ferreira@bancointer.com.br>
2021-08-15 15:06:16 -0300

    Add exe03

--- exe03/functions.c
+++ exe03/functions.c
@@ -1,7 +1,20 @@
-
 #include "functions.h"
 
-int somar(int n1, int n2){
+void menor_maior(int v[], int n, int *menor, int *maior)
+{
+    *menor = v[0];
+    *maior = v[0];
 
-    return n1+n2;
+    // Loop pelo vetor
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] < *menor)
+        {
+            *menor = v[i];
+        }
+        else if (v[i] > *maior)
+        {
+            *maior = v[i];
+        }
+    }
 }
